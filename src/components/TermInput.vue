43c9e9ef7c4cc8c6f0d50d7e12720e6dba65bbb0
Andre Valdestilhas <valdestilhas@informatik.uni-leipzig.de>
2020-08-27 01:21:43 +0200

    improvement of prefix.cc autocomplete

--- src/components/TermInput.vue
+++ src/components/TermInput.vue
@@ -13,7 +13,7 @@
     <input type="text" class="form-control" v-model="prefix" @input="filterprefixs"
            @focus="modal = true">
 
-    <div v-if="filteredprefixs && modal">
+    <div v-if="filteredprefixs && modal" class="options" ref="optionsList">
       <ul class="w-48 bg-gray-800 text-black">
         <li v-for="filteredprefix in this.filteredprefixs"
             class="py-2 border-b cursor-pointer" :key="filteredprefix"
@@ -162,3 +162,86 @@ export default {
 }
 
 </script>
+
+<style scoped>
+  .autocomplete {
+    width: 100%;
+    position: relative;
+  }
+  .input {
+    height: 40px;
+    border-radius: 3px;
+    border: 2px solid lightgray;
+    box-shadow: 0 0 10px #eceaea;
+    font-size: 25px;
+    padding-left: 10px;
+    padding-top: 10px;
+    cursor: text;
+  }
+  .close {
+    position: absolute;
+    right: 2px;
+    top: 4px;
+    background: none;
+    border: none;
+    font-size: 30px;
+    color: lightgrey;
+    cursor: pointer;
+  }
+  .placeholder {
+    position: absolute;
+    top: 11px;
+    left: 11px;
+    font-size: 25px;
+    color: #d0d0d0;
+    pointer-events: none;
+  }
+  .popover {
+    min-height: 50px;
+    border: 2px solid lightgray;
+    position: absolute;
+    top: 46px;
+    left: 0;
+    right: 0;
+    background: #fff;
+    border-radius: 3px;
+    text-align: center;
+  }
+  .popover input {
+    width: 95%;
+    margin-top: 5px;
+    height: 40px;
+    font-size: 16px;
+    border-radius: 3px;
+    border: 1px solid lightgray;
+    padding-left: 8px;
+  }
+  .options {
+    max-height: 150px;
+    overflow-y: scroll;
+    margin-top: 5px;
+  }
+  .options ul {
+    list-style-type: none;
+    text-align: left;
+    padding-left: 0;
+  }
+  .options ul li {
+    border-bottom: 1px solid lightgray;
+    padding: 10px;
+    cursor: pointer;
+    background: #f1f1f1;
+  }
+  .options ul li:first-child {
+    border-top: 2px solid #d6d6d6;
+  }
+  .options ul li:not(.selected):hover {
+    background: #8c8c8c;
+    color: #fff;
+  }
+  .options ul li.selected {
+    background: #58bd4c;
+    color: #fff;
+    font-weight: 600;
+  }
+</style>
